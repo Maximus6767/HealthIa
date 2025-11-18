cb478d22e1aa54dcb6924ccbea2fe50c3523126c
Felipe Maximus Ribeiro Rodrigues <Felipe Maximus Ribeiro Rodrigues>
2025-11-18 17:14:00 -0300

    Commitando meu projeto na escola

--- backend/index.js
+++ backend/index.js
@@ -15,53 +15,73 @@ app.listen(porta, () => {
 })
 
 
+//criando rota que mostre todas as instituições salvas no banco
+app.get("/instituicoes", async (req,res)=>{
+    try {
+        const [resultado] = await conexao.query("SELECT * FROM instituicoes")
+
+        res.json(resultado)
+
+    } catch (error) {
+        console.log(`O erro foi: ${error}`)
+    }
+})
+
+
+
 //criando a rota do contate-nos
 app.post("/contate_nos", async (req, res) => {
 
     try {
-        const { nome, email, instituicao, assunto, mensagem } = req.body
+        const { nome, email, codigo_instituicao, assunto, mensagem } = req.body
 
         //minhas variaveis sem ter espaço 
         const nomeTrim = nome?.trim();
         const emailTrim = email?.trim();
-        const instituicaoTrim = instituicao?.trim();
         const assuntoTrim = assunto?.trim();
         const mensagemTrim = mensagem?.trim();
 
 
-        //tradando erros dos usuario sacana
+
+        //tratando erros dos usuario sacana
         if (!nomeTrim || nomeTrim.length < 10) {
             return res.json({
                 "resposta": " Insira seu nome e sobrenome para entrar na lista de contato."
             })
+
         } else if (!emailTrim) {
             return res.json({
                 "resposta": " O email deve ser preenchido para entrarmos em contato."
+            })
+            
+        } else if (!emailTrim) {
+            return res.json({
+                "resposta": " Escolha uma de nossas instituições."
             })
         } else if (!mensagemTrim) {
             return res.json({
                 "resposta": " Você deve escrever sua mensagem para te ajudarmos"
             })
+            
         } else if (mensagemTrim.length < 30) {
             return res.json({
                 "resposta": "Mensagens devem conter pelo menos 30 caracteres."
             })
+
         } else if (assuntoTrim.length < 10) {
             return res.json({
                 "resposta": " Detalhe sobre o que gostaria de falar."
             })
+
         } else if (!assuntoTrim) {
             return res.json({
                 "resposta": " Você deve dizer sobre o que se trata a mensagem na qual deseja nos contatar."
             })
-        } else if (!instituicaoTrim) {
-            return res.json({
-                "resposta": " Insira o nome de uma de nossas instituições."
-            })
+
         }
         // Fazendo meu insertzin no bancos dos dados
-        const sql = `INSERT INTO contate_nos (nome,email,instituicao,assunto,mensagem) VALUES (?,?,?,?,?)`
-        const [resultado] = await conexao.query(sql, [nomeTrim, emailTrim, instituicaoTrim, assuntoTrim, mensagemTrim])
+        const sql = `INSERT INTO contate_nos (nome,email,codigo_instituicao,assunto,mensagem) VALUES (?,?,?,?,?)`
+        const [resultado] = await conexao.query(sql,[nomeTrim, emailTrim,codigo_instituicao, assuntoTrim, mensagemTrim])
 
         //tratando a resposta
         if (resultado.affectedRows == 1) {
@@ -73,9 +93,12 @@ app.post("/contate_nos", async (req, res) => {
             return res.json({
                 "resposta": " Erro inesperado "
             })
-
+        
 
         }
+
+
+
     } catch (error) {
         res.json({
             "resposta": error
