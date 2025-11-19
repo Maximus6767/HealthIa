04f1b1043e9c5ddf9e57df0e064ddbd0f3fd341d
Maximus6767 <lipepr4yx@gmail.com>
2025-11-18 22:59:33 -0300

    Projeto quase finalizado commit em casa.

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-const myslq2 = require('mysql2/promise')
-const porta = 3000
-const app = express()
-const conexao = require('./db.js')
+const express = require('express');
+const cors = require('cors');
+const myslq2 = require('mysql2/promise');
+const porta = 3000;
+const app = express();
+const conexao = require('./db.js');
 
 //permissão para acessar os dados do body
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //mensagem para saber que o server está no ar
 app.listen(porta, () => {
-    console.log('Servidor pocandissimo')
-})
+    console.log('Servidor pocandissimo');
+});
 
 
 //criando rota que mostre todas as instituições salvas no banco
 app.get("/instituicoes", async (req,res)=>{
     try {
-        const [resultado] = await conexao.query("SELECT * FROM instituicoes")
-
-        res.json(resultado)
-
+        const [resultado] = await conexao.query("SELECT cod_instituicao, nome_instituicao FROM instituicoes");
+        res.json(resultado);
     } catch (error) {
-        console.log(`O erro foi: ${error}`)
-    }
-})
-
-
+        console.log(`O erro foi: ${error}`);
+    };
+});
 
 //criando a rota do contate-nos
 app.post("/contate_nos", async (req, res) => {
 
     try {
-        const { nome, email, codigo_instituicao, assunto, mensagem } = req.body
+        const { nome, email, codigo_instituicao, assunto, mensagem } = req.body;
 
         //minhas variaveis sem ter espaço 
         const nomeTrim = nome?.trim();
@@ -47,61 +43,57 @@ app.post("/contate_nos", async (req, res) => {
         if (!nomeTrim || nomeTrim.length < 10) {
             return res.json({
                 "resposta": " Insira seu nome e sobrenome para entrar na lista de contato."
-            })
+            });
 
         } else if (!emailTrim) {
             return res.json({
                 "resposta": " O email deve ser preenchido para entrarmos em contato."
+            });
+            
+        } else if (!codigo_instituicao || codigo_instituicao.trim() === ""){
+            return res.status(400).json({
+                "resposta":"Escolha uma das instituições."
             })
             
-        } else if (!emailTrim) {
+        } else if (assuntoTrim.length < 10) {
             return res.json({
-                "resposta": " Escolha uma de nossas instituições."
-            })
+                "resposta": " Detalhe sobre o que gostaria de falar."
+            });
+            
+        } else if (!assuntoTrim) {
+            return res.json({
+                "resposta": " Você deve dizer sobre o que se trata a mensagem na qual deseja nos contatar."
+            });
         } else if (!mensagemTrim) {
             return res.json({
                 "resposta": " Você deve escrever sua mensagem para te ajudarmos"
-            })
+            });
             
         } else if (mensagemTrim.length < 30) {
             return res.json({
                 "resposta": "Mensagens devem conter pelo menos 30 caracteres."
-            })
-
-        } else if (assuntoTrim.length < 10) {
-            return res.json({
-                "resposta": " Detalhe sobre o que gostaria de falar."
-            })
-
-        } else if (!assuntoTrim) {
-            return res.json({
-                "resposta": " Você deve dizer sobre o que se trata a mensagem na qual deseja nos contatar."
-            })
-
+            });
+            
         }
         // Fazendo meu insertzin no bancos dos dados
-        const sql = `INSERT INTO contate_nos (nome,email,codigo_instituicao,assunto,mensagem) VALUES (?,?,?,?,?)`
-        const [resultado] = await conexao.query(sql,[nomeTrim, emailTrim,codigo_instituicao, assuntoTrim, mensagemTrim])
+        const sql = `INSERT INTO contate_nos (nome,email,codigo_instituicao,assunto,mensagem) VALUES (?,?,?,?,?)`;
+        const [resultado] = await conexao.query(sql,[nomeTrim, emailTrim,codigo_instituicao, assuntoTrim, mensagemTrim]);
 
         //tratando a resposta
         if (resultado.affectedRows == 1) {
 
             return res.json({
                 "resposta": " Mensagem enviada aguarde nossa resposta no email! "
-            })
+            });
         } else {
             return res.json({
                 "resposta": " Erro inesperado "
-            })
+            });
         
-
-        }
-
-
-
+        };
     } catch (error) {
         res.json({
             "resposta": error
-        })
-    }
-})
+        });
+    };
+});
